03360f284cd0a50379014d1d1df8b76d12a4b74e
Krutik Shiroya <krutik.shiroya@joflee.com>
2022-05-17 09:49:15 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,37 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Demo App")),
-      body: Container(
-        child: const Center(child: Text("Hii Krutik")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.blueGrey,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.orange,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
